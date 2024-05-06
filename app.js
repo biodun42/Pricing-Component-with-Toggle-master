1e52bf6daf7e847e58f4c11dfcf7d824323861eb
Muhammed Abiodun <muhammedabiodun42@gmail.com>
2024-05-06 18:15:07 +0100

    commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const buttonOne = document.getElementById('one');
 const buttonTwo = document.getElementById('two');
+const container = document.querySelector('.toggle-master');
 
 
 function changeOne(){
@@ -8,6 +9,7 @@ function changeOne(){
     document.getElementById('textone').innerText = '$19.99';
     document.getElementById('texttwo').innerText = '$24.99';
     document.getElementById('textthree').innerText = '$39.99';
+    container.style.backgroundColor = 'black';
 }
 
 function changeTwo(){
@@ -16,4 +18,5 @@ function changeTwo(){
     document.getElementById('textone').innerText = '$199.99';
     document.getElementById('texttwo').innerText = '$249.99';
     document.getElementById('textthree').innerText = '$399.99';
+    container.style.backgroundColor = 'hsl(236, 72%, 79%)';
 }
